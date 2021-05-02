e7d9b9b95e8733df527dc7517dd487b35a62a44a
Ethan Soo Hon <ethanny2@gmail.com>
2021-05-02 17:14:40 -0400

    Added in sanity stuido CMS backend with blog starter. Both in this one monorepo. Learned how to add new content via Sanity GUI, how to add a new schema definition to my sanity backend, how to write GROQ query with the vscode-sanity extension which can run the qureies in a vscode tab to see if they work. Integrated a stripe shopping cart using the use-shopping-cart library with products sourced from my Sanity backend

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,32 +1,38 @@
-import Link from "next/link";
-import { client } from "../lib/sanity/client";
-import { homeQuery } from "../lib/sanity/homeQuery";
+import Link from 'next/link';
+import { client } from '../lib/sanity/client';
+import { homeQuery } from '../lib/sanity/homeQuery';
+
 export default function Home({ posts }) {
-  return (
-    <div>
-      <main>
-        <h1>My Blog</h1>
-        <hr />
-        <ul>
-          {posts.map((p) => (
-            <li key={p._id}>
-              <Link href={`/posts/${p.slug}`}>
-                <a>{p.title}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </main>
-    </div>
-  );
+	return (
+		<div>
+			<main>
+				<h1>
+					My Blog and{' '}
+					<Link href='/merch'>
+						<a>Merch Store</a>
+					</Link>
+				</h1>
+				<hr />
+				<ul>
+					{posts.map((p) => (
+						<li key={p._id}>
+							<Link href={`/posts/${p.slug}`}>
+								<a>{p.title}</a>
+							</Link>
+						</li>
+					))}
+				</ul>
+			</main>
+		</div>
+	);
 }
 
 export async function getStaticProps({ params }) {
-  const posts = await client.fetch(homeQuery);
+	const posts = await client.fetch(homeQuery);
 
-  return {
-    props: {
-      posts,
-    },
-  };
+	return {
+		props: {
+			posts
+		}
+	};
 }
